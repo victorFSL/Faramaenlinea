ee137ae8ba612df2298d3313f54a59e554d4c405
Victor Santana <santanaebay@gmail.com>
2017-05-15 22:18:46 -0400

    Fix seed

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,5 +3,5 @@ class Location < ApplicationRecord
   has_many :drugs, through: :prices
   has_many :drug_stores, through: :prices
 
-  enum state_number: [ :santiago, :santo_domingo]
+  enum state_number: [ :santiago_de_los_caballeros, :santo_domingo] unless instance_methods.include? :state_number
 end
